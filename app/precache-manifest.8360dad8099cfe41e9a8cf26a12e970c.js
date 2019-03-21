5498d3562c145e551f9d1830d9d5b323c07e942a
Artur Z <artur.zahorski@gmail.com>
2019-03-21 15:16:15 +0100

    Add compiled files for github pages

---
+++ app/precache-manifest.8360dad8099cfe41e9a8cf26a12e970c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fb0e721603483ea9958c",
+    "url": "/static/js/main.6a17d23d.chunk.js"
+  },
+  {
+    "revision": "6ee2a076a9110d1e0f16",
+    "url": "/static/js/2.a17300e0.chunk.js"
+  },
+  {
+    "revision": "fb0e721603483ea9958c",
+    "url": "/static/css/main.e943471f.chunk.css"
+  },
+  {
+    "revision": "af28f94dbc1a28c5f830aa7b424f2c11",
+    "url": "/index.html"
+  }
+];
