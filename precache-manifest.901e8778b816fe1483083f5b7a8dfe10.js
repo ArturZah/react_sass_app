8918bc6bf81b28943922cb15a6e27cf91d95b9fd
Artur Zahorski <artur.zahorski@am.edu.pl>
2019-03-21 15:31:04 +0100

    Updates

---
+++ precache-manifest.901e8778b816fe1483083f5b7a8dfe10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7e1ca6984c7c081cdf9",
+    "url": "/react_sass_app/static/js/runtime~main.3c0cf978.js"
+  },
+  {
+    "revision": "fb0e721603483ea9958c",
+    "url": "/react_sass_app/static/js/main.6a17d23d.chunk.js"
+  },
+  {
+    "revision": "6ee2a076a9110d1e0f16",
+    "url": "/react_sass_app/static/js/2.a17300e0.chunk.js"
+  },
+  {
+    "revision": "fb0e721603483ea9958c",
+    "url": "/react_sass_app/static/css/main.e943471f.chunk.css"
+  },
+  {
+    "revision": "7411772786644bc51a4e501e1c993e15",
+    "url": "/react_sass_app/index.html"
+  }
+];
